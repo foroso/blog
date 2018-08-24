492e99540e1934f9b60ccbe0cd705dbb1032f2c9
zhangt <you@example.com>
2018-08-24 17:44:56 +0800

    finish static pages

---
+++ resources/views/static_pages/help.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+@section('title', '我的帮助页')
+@section('content')
+    <h1>帮助页面</h1>
+@stop
---
+++ resources/views/static_pages/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+@section('title', '我的首页')
+@section('content')
+    <h1>首页</h1>
+@stop
