19d8e2e1cf495d1929f2a6c5f8488475f46fddc3
zhangt <you@example.com>
2019-03-11 17:49:17 +0800

    新增修改和删除收货地址功能

--- app/Http/Controllers/UserAddressesController.php
+++ app/Http/Controllers/UserAddressesController.php
@@ -17,11 +17,22 @@ class UserAddressesController extends Controller
     }
 
     /**
+     * 创建收货地址
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
     public function create()
     {
         return view('user_addresses.create_and_edit', ['address' => new UserAddress()]);
+    }
+
+    /**
+     * 修改收货地址
+     * @param UserAddress $user_address
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function edit(UserAddress $user_address)
+    {
+        return view('user_addresses.create_and_edit', ['address' => $user_address]);
     }
 
     /**
@@ -43,4 +54,36 @@ class UserAddressesController extends Controller
         return redirect()->route('user_addresses.index');
     }
 
+    /**
+     * 修改收货地址
+     * @param UserAddress $user_address
+     * @param UserAddressRequest $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function update(UserAddress $user_address, UserAddressRequest $request)
+    {
+        $user_address->update($request->only([
+            'province',
+            'city',
+            'district',
+            'address',
+            'zip',
+            'contact_name',
+            'contact_phone',
+        ]));
+
+        return redirect()->route('user_addresses.index');
+    }
+
+    /**
+     * 删除收货地址
+     * @param UserAddress $user_address
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function destroy(UserAddress $user_address)
+    {
+        $user_address->delete();
+
+        return redirect()->route('user_addresses.index');
+    }
 }
